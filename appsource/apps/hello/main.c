8be61879bb9a51781e6387fec016c3a71c8f1802
Manojkiran Eda <manojkiran.eda@gmail.com>
2026-08-28 18:03:56 +0530

    Disable PIC for TAP firmware

--- appsource/apps/hello/main.c
+++ appsource/apps/hello/main.c
@@ -1,13 +1,17 @@
 #include "sbe/format.h"
 
 /* Locate these symbols in the ELF/map file and inspect them after execution. */
-volatile char sbe_test_output[64];
+volatile char* sbe_test_output = (volatile char*) 0xFFF60000 + (0x400 * 16);
 volatile int sbe_test_output_length;
 
 int main(void)
 {
     sbe_test_output_length = sbe_snprintf((char *)sbe_test_output,
-                                          sizeof(sbe_test_output),
+                                           1024,
                                           "Hello world");
+    while (1)
+    {
+        // Infinite loop
+    }
     return 0;
 }
